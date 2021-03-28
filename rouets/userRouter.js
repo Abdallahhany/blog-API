0adfa87195b7668d4065e66e57bcdaca2bc2c6f8
abdallah hany <Abdallahhany253@gmail.com>
2021-03-28 23:32:17 +0200

    changes

--- rouets/userRouter.js
+++ rouets/userRouter.js
@@ -6,7 +6,7 @@ const middleware = require('../middleware/middleware');
 UserRouter.get('/',userController.Users);
 UserRouter.get('/:username', middleware.checkToken ,userController.getUser);
 UserRouter.post('/signup',userController.signUp);
-UserRouter.patch('/update/:username',middleware.checkToken,userController.UpdatePassword);
+UserRouter.patch('/update/:username',userController.UpdatePassword);
 UserRouter.delete('/:username',middleware.checkToken,userController.deleteAccount);
 UserRouter.post('/login',userController.login);
 UserRouter.get('/checkusername/:username',userController.checkUserName);
